9c396c77b4392a495b11817587b62cad0e41896b
Giuseppe <Giuseppe@192.168.1.222>
2021-01-16 15:20:33 +0100

    ??

--- ProgettoINGSW/src/jdbcModels/SongJDBC.java
+++ ProgettoINGSW/src/jdbcModels/SongJDBC.java
@@ -32,8 +32,9 @@ public class SongJDBC implements SongDao
 	{
 		Connection connection = this.dataSource.getConnection();
 
-		String query = "select album.albumid, song.name as songname, album.name as albumname, song.link, song.decription, song.users, song.length"
-				+ " from song" + " inner join album" + " on song.album = album.albumid" + " where song.name = ?";
+		String query = "select album.albumid, song.name as songname, album.name as albumname,"
+				+ " song.link, song.decription, song.users, song.length, song.rating, song.postdate" + " from song"
+				+ " inner join album" + " on song.album = album.albumid" + " where song.name = ?";
 
 		ArrayList<Song> songs = new ArrayList<Song>();
 
@@ -48,7 +49,6 @@ public class SongJDBC implements SongDao
 		statment.close();
 		result.close();
 		connection.close();
-
 		return songs;
 	}
 
@@ -57,15 +57,14 @@ public class SongJDBC implements SongDao
 	{
 		Connection connection = this.dataSource.getConnection();
 
-		String query = "select album.albumid, song.name as songname, album.name as albumname, song.link, song.decription, song.users, song.length"
-				+ " from song" + " inner join album" + " on song.album = album.albumid"
-				+ " where song.name = ? and song.album = ?";
+		String query = "select album.albumid, song.name as songname, album.name as albumname,"
+				+ " song.link, song.decription, song.users, song.length, song.rating, song.postdate" + " from song"
+				+ " inner join album" + " on song.album = album.albumid" + " where song.name = ? and song.album = ?";
 		PreparedStatement statment = connection.prepareStatement(query);
 		statment.setString(1, name);
 		statment.setInt(2, albumKey);
 		ResultSet result = statment.executeQuery();
 
-		connection.close();
 		Song song = null;
 		while (result.next())
 		{
@@ -74,31 +73,37 @@ public class SongJDBC implements SongDao
 
 		if (song != null)
 		{
+			connection.close();
 			return song;
 		} else
 		{
 			throw new RuntimeException("No song in this album with this name");
 		}
-
 	}
 
 	private static Song buildSong(ResultSet result) throws SQLException
 	{
 		String songName = result.getString("songname");
 		String albumName = result.getString("albumname");
-		int albumid = result.getInt("albumid");
+		int albumID = result.getInt("albumid");
 		String link = result.getString("link");
 		String description = result.getString("decription");
 		String user = result.getString("users");
 		float length = result.getFloat("length");
+		float rating = result.getFloat("rating");
+		Date postDate = result.getDate("postdate");
 
 		Song song = new Song();
 		song.setName(songName);
-		song.setAlbum(new Pair<Integer, String>(albumid, albumName));
+		song.setAlbumID(albumID);
+		song.setAlbumName(albumName);
 		song.setLink(link);
 		song.setDescription(description);
 		song.setUser(user);
 		song.setLength(length);
+		song.setRating(rating);
+		song.setPostDate(postDate);
+
 		return song;
 	}
 
@@ -111,7 +116,7 @@ public class SongJDBC implements SongDao
 
 		PreparedStatement statment = connection.prepareStatement(query);
 		statment.setString(1, song.getName());
-		statment.setInt(2, song.getAlbum().key);
+		statment.setInt(2, song.getAlbumID());
 		statment.setString(3, song.getLink());
 		statment.setString(4, song.getDescription());
 		statment.setString(5, userNickname);
@@ -127,14 +132,14 @@ public class SongJDBC implements SongDao
 	{
 		Connection connection = this.dataSource.getConnection();
 
-		String query = "update song set album = ?, link = ?, decription = ?, length = ? where name = ? and album = ?";
+		String query = "update song set link = ?, decription = ?, length = ? where name = ? and album = ?";
 		PreparedStatement statment = connection.prepareStatement(query);
-		statment.setInt(1, song.getAlbum().key);
+		statment.setInt(1, song.getAlbumID());
 		statment.setString(2, song.getLink());
 		statment.setString(3, song.getDescription());
 		statment.setFloat(4, song.getLength());
 		statment.setString(5, song.getName());
-		statment.setInt(6, song.getAlbum().key);
+		statment.setInt(6, song.getAlbumID());
 
 		statment.executeUpdate();
 		statment.close();
@@ -149,7 +154,7 @@ public class SongJDBC implements SongDao
 		String query = "delete from song where name = ? and album = ?";
 		PreparedStatement statment = connection.prepareStatement(query);
 		statment.setString(1, song.getName());
-		statment.setInt(2, song.getAlbum().key);
+		statment.setInt(2, song.getAlbumID());
 		statment.execute();
 		statment.close();
 		connection.close();
@@ -165,6 +170,8 @@ public class SongJDBC implements SongDao
 	@Override
 	public ArrayList<SongReview> getReviews(Song song) throws SQLException
 	{
+		Connection connection = this.dataSource.getConnection();
+
 		String query = "select users, song, album, numberofstars, description, postdate " + "from song_review "
 				+ "where song = ? and album = ?";
 
@@ -181,12 +188,14 @@ public class SongJDBC implements SongDao
 
 		result.close();
 		statment.close();
+		connection.close();
 
 		return reviews;
 	}
 
 	private SongReview buildSongReview(ResultSet result) throws SQLException
 	{
+
 		String user = result.getString("users");
 		String songTitle = result.getString("song");
 		int album = result.getInt("album");
@@ -208,6 +217,8 @@ public class SongJDBC implements SongDao
 	@Override
 	public ArrayList<Song> searchByKeyWords(String keyWords, int limit, int offset) throws SQLException
 	{
+		Connection connection = this.dataSource.getConnection();
+
 		String query = "select album.albumid, song.name as songname, album.name as albumname, song.users, song.rating"
 				+ " from song" + " inner join album" + " on song.album = album.albumid"
 				+ " where songname similar to ? or albumname similar to ?" + " limit ? offset ?";
@@ -241,6 +252,7 @@ public class SongJDBC implements SongDao
 
 		result.close();
 		statment.close();
+		connection.close();
 
 		return songs;
 	}
@@ -248,6 +260,8 @@ public class SongJDBC implements SongDao
 	@Override
 	public void addReview(SongReview review) throws SQLException
 	{
+		Connection connection = this.dataSource.getConnection();
+
 		String query = "insert into song_review(users,song,album,numberofstars,description) values(?,?,?,?,?)";
 		PreparedStatement statement = connection.prepareStatement(query);
 		statement.setString(1, review.getUser());
@@ -257,11 +271,14 @@ public class SongJDBC implements SongDao
 		statement.setString(5, review.getDescription());
 		statement.execute();
 		statement.close();
+		connection.close();
 	}
 
 	@Override
 	public void deleteReview(String nickname, String song, int albumId) throws SQLException
 	{
+		Connection connection = this.dataSource.getConnection();
+
 		String query = "delete from song_review where users = ? and song = ? and album = ?";
 		PreparedStatement statment = connection.prepareStatement(query);
 		statment.setString(1, nickname);
@@ -269,11 +286,14 @@ public class SongJDBC implements SongDao
 		statment.setInt(3, albumId);
 		statment.execute();
 		statment.close();
+		connection.close();
 	}
 
 	@Override
 	public void updateReview(SongReview review) throws SQLException
 	{
+		Connection connection = this.dataSource.getConnection();
+
 		String query = "update song_review set numberofstars = ?, description = ? "
 				+ "where users = ? and song = ? and album = ?";
 
@@ -285,6 +305,7 @@ public class SongJDBC implements SongDao
 		statment.setInt(5, review.getAlbumId());
 		statment.executeUpdate();
 		statment.close();
+		connection.close();
 	}
 
 	@Override
